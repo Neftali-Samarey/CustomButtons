fcf8e07c9437976a429d867599bc8884242b1abd
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-10-14 09:42:48 -0400

    Initial Commit

---
+++ CustomButtons/CustomButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonsApp.swift
+//  CustomButtons
+//
+//  Created by Neftali Samarey on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
