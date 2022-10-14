2cd94875fe6c5c0719a6233da4f1e2625f170322
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-10-14 10:15:28 -0400

    Added sample scaleEffect modifier

--- CustomButtons/Buttons/Modifiers/GradientButtonStyle.swift
+++ CustomButtons/Buttons/Modifiers/GradientButtonStyle.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Neftali Samarey on 10/14/22.
 //
+// Notes: configuration object allows us to interact with the internal label, isPressed, role instance properties
+//
 
 import SwiftUI
 
@@ -15,5 +17,6 @@ struct GradientButtonStyle: ButtonStyle {
             .padding()
             .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
             .cornerRadius(15)
+            .scaleEffect(configuration.isPressed ? 1.3 : 1.0)
     }
 }
