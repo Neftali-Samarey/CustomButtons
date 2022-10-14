71919b7f1de17099fcad663abecf5012c3b23298
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-10-14 10:19:15 -0400

    Initialized custom Button Style effect

---
+++ CustomButtons/Buttons/Modifiers/LoveReactionButtonStyle.swift
@@ -0,0 +1,16 @@
+//
+//  LoveReactionButtonStyle.swift
+//  CustomButtons
+//
+//  Created by Neftali Samarey on 10/14/22.
+//
+
+import SwiftUI
+
+/// Style effect: Will allow the button to react with an animation effect that eases out/in.
+struct LoveReactionButtonStyle: ButtonStyle {
+
+    func makeBody(configuration: Configuration) -> some View {
+        //
+    }
+}
